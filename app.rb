f8c69332d0e747ee5d79c55e281a5a08a20ffe49
Trevor Rosen <trevor@catapult-creative.com>
2011-11-13 17:14:59 -0600

    Pretty up the dashboard page

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require "sinatra"
-$:.unshift(File.expand_path(File.dirname(__FILE__) + "/lib"))
+$:.push(File.expand_path(File.dirname(__FILE__) + "/lib"))
 
 require "dashboard"
 
@@ -7,8 +7,7 @@ get "/dashboard/:id" do
 
  @test_flot = JSON.generate([[1,3.2], [2, 4.1], [2.3, 4.6]])
 
- comp = Dashboard::Composite.load_by_id(params[:id])
-
+ comp           = Dashboard::Composite.load_by_id(params[:id])
  @price_text    = comp.current_hog_price
  @pig_news      = comp.pig_news
  @sales_leaders = comp.sales_leaders
